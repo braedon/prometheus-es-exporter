f32de990045eaa6fc2de08ef46e4725df890250c
Braedon Vickers <braedon.vickers@gmail.com>
2016-11-20 23:17:49 +1300

    Update parser to produce new bucket label format

--- prometheus_es_exporter/parser.py
+++ prometheus_es_exporter/parser.py
@@ -1,4 +1,4 @@
-def parse_buckets(buckets, metric=[], labels={}):
+def parse_buckets(agg_key, buckets, metric=[], labels={}):
     result = []
 
     for index, bucket in enumerate(buckets):
@@ -6,47 +6,47 @@ def parse_buckets(buckets, metric=[], labels={}):
 
         if 'key' in bucket.keys():
             bucket_key = str(bucket['key'])
-            if 'bucket' in labels_next.keys():
-                labels_next['bucket'] = labels_next['bucket'] + [bucket_key]
+            if agg_key in labels_next.keys():
+                labels_next[agg_key] = labels_next[agg_key] + [bucket_key]
             else:
-                labels_next['bucket'] = [bucket_key]
+                labels_next[agg_key] = [bucket_key]
             del bucket['key']
         else:
             bucket_key = 'filter_' + str(index)
-            if 'bucket' in labels_next.keys():
-                labels_next['bucket'] = labels_next['bucket'] + [bucket_key]
+            if agg_key in labels_next.keys():
+                labels_next[agg_key] = labels_next[agg_key] + [bucket_key]
             else:
-                labels_next['bucket'] = [bucket_key]
+                labels_next[agg_key] = [bucket_key]
 
-        result.extend(parse_agg(bucket, metric=metric, labels=labels_next))
+        result.extend(parse_agg(bucket_key, bucket, metric=metric, labels=labels_next))
 
     return result
 
-def parse_buckets_fixed(buckets, metric=[], labels={}):
+def parse_buckets_fixed(agg_key, buckets, metric=[], labels={}):
     result = []
 
     for bucket_key, bucket in buckets.items():
         labels_next = labels.copy()
 
-        if 'bucket' in labels_next.keys():
-            labels_next['bucket'] = labels_next['bucket'] + [bucket_key]
+        if agg_key in labels_next.keys():
+            labels_next[agg_key] = labels_next[agg_key] + [bucket_key]
         else:
-            labels_next['bucket'] = [bucket_key]
+            labels_next[agg_key] = [bucket_key]
 
-        result.extend(parse_agg(bucket, metric=metric, labels=labels_next))
+        result.extend(parse_agg(bucket_key, bucket, metric=metric, labels=labels_next))
 
     return result
 
-def parse_agg(agg, metric=[], labels={}):
+def parse_agg(agg_key, agg, metric=[], labels={}):
     result = []
 
     for key, value in agg.items():
         if key == 'buckets' and isinstance(value, list):
-            result.extend(parse_buckets(value, metric=metric, labels=labels))
+            result.extend(parse_buckets(agg_key, value, metric=metric, labels=labels))
         elif key == 'buckets' and isinstance(value, dict):
-            result.extend(parse_buckets_fixed(value, metric=metric, labels=labels))
+            result.extend(parse_buckets_fixed(agg_key, value, metric=metric, labels=labels))
         elif isinstance(value, dict):
-            result.extend(parse_agg(value, metric=metric + [key], labels=labels))
+            result.extend(parse_agg(key, value, metric=metric + [key], labels=labels))
         else:
             result.append((metric + [key], labels, value))
 
@@ -60,6 +60,6 @@ def parse_response(response, metric=[]):
 
         if 'aggregations' in response.keys():
             for key, value in response['aggregations'].items():
-                result.extend(parse_agg(value, metric=metric + [key]))
+                result.extend(parse_agg(key, value, metric=metric + [key]))
 
     return result
