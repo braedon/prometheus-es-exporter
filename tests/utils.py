1dc62ec0ec036f4a672fbca36e7f036347bc0f53
Braedon Vickers <braedon.vickers@gmail.com>
2017-01-20 00:27:02 +1300

    Move test utils into their own file

---
+++ tests/utils.py
@@ -0,0 +1,28 @@
+from prometheus_es_exporter import format_metric_name, format_label_value
+
+
+def format_label(key, value_list):
+    return key + '="' + format_label_value(value_list) + '"'
+
+
+def format_metric(name_list, label_dict):
+    name = format_metric_name(name_list)
+
+    if len(label_dict) > 0:
+        sorted_keys = sorted(label_dict.keys())
+        labels = '{'
+        labels += ','.join([format_label(k, label_dict[k]) for k in sorted_keys])
+        labels += '}'
+    else:
+        labels = ''
+
+    return name + labels
+
+
+# Converts the parse_response() result into a psuedo-prometheus format
+# that is useful for comparing results in tests.
+def convert_result(result):
+    return {
+        format_metric(name_list, label_dict): value
+        for (name_list, label_dict, value) in result
+    }
