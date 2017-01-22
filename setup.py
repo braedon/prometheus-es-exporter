639feadb3364a3908c678d1670a595d56187fc6d
Braedon Vickers <braedon.vickers@gmail.com>
2017-01-23 11:45:51 +1300

    Release 0.3.x Alpha2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prometheus-es-exporter',
-    version='0.3.0.alpha1',
+    version='0.3.0.alpha2',
     description='Elasticsearch query Prometheus exporter',
     url='https://github.com/Braedon/prometheus-es-exporter',
     author='Braedon Vickers',
