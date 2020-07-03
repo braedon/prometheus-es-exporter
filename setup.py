3badc12783fc8e89f70e1d08f66f00bc1219d7b8
Braedon Vickers <braedon.vickers@gmail.com>
2020-07-03 19:23:55 +0800

    Release 0.11.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='prometheus-es-exporter',
-    version='0.11.0.dev1',
+    version='0.11.0',
     description='Elasticsearch query Prometheus exporter',
     long_description=long_description,
     long_description_content_type='text/markdown',
